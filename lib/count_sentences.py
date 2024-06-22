3c95903439d9348215f0c3b3951490cfc4d5c9ac
Samkibe <kibesam433@gmail.com>
2024-06-22 22:58:44 +0300

    sentences counted

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,43 @@
 
 class MyString:
   pass
+  #default value of empty string
+  def __init__(self, value = ''):
+    self._value = value
+
+  # give a value property as an attribute of this class
+  def get_value(self):
+    return self._value
+
+  def set_value(self, stringValue):
+    if(type(stringValue) == str):
+      self._value = stringValue
+
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+
+  # count of sentences in value
+  def count_sentences(self):
+    # Get the text stored in the 'value' attribute of the class instance
+    value = self.value
+
+    # Replacing '!' and '?' with '.'
+    value = value.replace('!', '.').replace('?', '.')
+
+    # Split the text into sentences based on period ('.') and remove empty strings
+    sentences = list(filter(None, value.split('.')))
+
+    # Return the count of sentences
+    return len(sentences)
+
